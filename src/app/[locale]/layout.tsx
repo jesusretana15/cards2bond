14e8fd109532a0f54b9ef5e0ef4243c4716a9e4e
Jesus Retana Artavia <jesus@Jesuss-MacBook-Air.local>
2024-11-07 23:08:01 -0600

    added meta desc

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import {routing} from '@/i18n/routing';
 import './globals.css';
 import { ReactNode } from 'react';
 import { Analytics } from "@vercel/analytics/react"
+import Head from 'next/head';
 
 type Props = {
   children: ReactNode;
@@ -13,6 +14,12 @@ type Props = {
 
 export default async function LocaleLayout({children}: Props) {
   const locale = await getLocale();
+  // Meta descriptions based on language
+  const metaDescription =
+    locale === 'es'
+      ? 'Conecta con amigos, familiares y hasta desconocidos con preguntas diseñadas para iniciar conversaciones auténticas y divertidas. Encuentra el tema perfecto para cada ocasión y fortalece tus relaciones de manera fácil y entretenida.'
+      : 'Connect with friends, family, and even strangers with questions designed to spark authentic and fun conversations. Find the perfect topic for any occasion and strengthen your relationships in an easy and entertaining way.';
+
   // Providing all messages to the client
   // side is the easiest way to get started
     // Ensure that the incoming `locale` is valid
@@ -24,9 +31,12 @@ export default async function LocaleLayout({children}: Props) {
 
   return (
     <html lang={locale}>
-      <head>
+        <Head>
         <title>cards2bond</title>
-      </head>
+        <meta name="description" content={metaDescription} />
+        <link rel="alternate" hrefLang="en" href="https://yourwebsite.com/en" />
+        <link rel="alternate" hrefLang="es" href="https://yourwebsite.com/es" />
+      </Head>
       <body
       >
         <NextIntlClientProvider messages={messages}>
